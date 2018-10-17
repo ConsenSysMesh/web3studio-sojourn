b2397e666b6e326ef353580c7c761b09937b1c33
Hadas Zeilberger <hadaszeilberger@Hadass-MacBook-Pro.local>
2018-10-17 15:33:34 -0400

    detox setup

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View testID="welcome" style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
