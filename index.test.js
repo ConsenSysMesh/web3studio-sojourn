81aed5ba43ae68f7c0cea5e06751f285bed24fdb
Hadas Zeilberger <hadaszeilberger@Hadass-MacBook-Pro.local>
2018-10-20 14:36:06 -0400

    reset index.js in cleanup of detox tests

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
-import RCTAesTests from './e2e/TestApp/RCTAesTests';
+import RCTAesTests from './src/RCTAesTests';
 AppRegistry.registerComponent(appName, () => RCTAesTests);
