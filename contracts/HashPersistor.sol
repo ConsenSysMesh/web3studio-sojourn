ee86903d4c69229932f57bce3b09f83ffdfdb602
Michael Barlock <barlockm@gmail.com>
2018-10-26 14:24:24 -0400

    Add NatSpec docs for HashPersistor contract

--- contracts/HashPersistor.sol
+++ contracts/HashPersistor.sol
@@ -1,13 +1,29 @@
 pragma solidity ^0.4.24;
 
+/**
+* @title A persistor of hashes per user with timestamps
+*/
 contract HashPersistor {
-  // sender => (hash => timestamp)
+  /**
+  * @notice Storage of hashes. Stored as Sender => (Hash => Timestamp)
+  */
   mapping(address => mapping(uint => uint)) public hashes;
 
+  /**
+  * @notice Saves the hash of a content
+  *
+  * @param contentHash - 256bit hash of some content
+  */
   function save(uint contentHash) public {
     hashes[msg.sender][contentHash] = block.timestamp;
   }
 
+  /**
+  * @notice Fetches the block timestamp for when a hash was saved
+  *
+  * @param contentHash - 256bit hash of some content
+  * @return timestamp as seconds since unix epoch
+  */
   function getTimestamp (uint contentHash) public view returns(uint) {
     return hashes[msg.sender][contentHash];
   }
