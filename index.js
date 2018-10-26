c762ff91acc46606dfdc8d98b5318b444ed5fc32
Michael Barlock <barlock@users.noreply.github.com>
2018-10-26 14:45:28 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry, YellowBox } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
-
-YellowBox.ignoreWarnings([
-  'Remote debugger is in a background tab which may cause apps to perform'
-]);
-
-AppRegistry.registerComponent(appName, () => App);
